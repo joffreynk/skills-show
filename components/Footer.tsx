c8e5dfe300b2f084d858421c1e450379b320a4f5
Joffreynk <joyjoffrey@gmail.com>
2023-07-01 22:18:38 +0200

    create reusable footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,12 @@
+import { footerLinks } from "@/constants";
 import Image from "next/image";
+
+const FooterColumn = ()=>(<div className="footer_column">
+  <h4 className="font-semibold">Title</h4>
+  <ul className="flex flex-col gap-2 font-normal">
+    links
+  </ul>
+</div>);
 
 const Footer = ()=>{
   return (
@@ -6,10 +14,13 @@ const Footer = ()=>{
       <div className="flex flex-col gap-12 w-full">
         <div className="flex items-start flex-col">
           <Image alt="show your skills" src="/logo-purple.svg" width={115} height={38}/>
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            SHow skills is the world's leading community for creating and share your skills with talents ad stakeholders
+          </p>
         </div>
-        <p className="text-start text-sm font-normal mt-5 max-w-xs">
-          SHow skills is the world's leading community for creating and share your skills with talents ad stakeholders
-        </p>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+        </div>
       </div>
     </footer>
   )
