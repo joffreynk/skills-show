ec1d6c8281b90a39437f28ea32d5fb992dad3878
Joffreynk <joyjoffrey@gmail.com>
2023-07-01 22:01:01 +0200

    add the first part of footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 const Footer = ()=>{
   return (
-    <div>
-        Footer
-    </div>
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image alt="show your skills" src="/logo-purple.svg" width={115} height={38}/>
+        </div>
+        <p className="text-start text-sm font-normal mt-5 max-w-xs">
+          SHow skills is the world's leading community for creating and share your skills with talents ad stakeholders
+        </p>
+      </div>
+    </footer>
   )
 }
 
