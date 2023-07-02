3d902d6116fec29f2eb8c21a99b54b6c7c87b33c
Joffreynk <joyjoffrey@gmail.com>
2023-07-02 10:33:39 +0200

    add all footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,18 @@
 import { footerLinks } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
 
-const FooterColumn = ()=>(<div className="footer_column">
-  <h4 className="font-semibold">Title</h4>
+type FooterColumnProps = {
+  title: string;
+  links: string[];
+}
+
+const FooterColumn = ({title, links}: FooterColumnProps)=>(<div className="footer_column">
+  <h4 className="font-semibold">{title}</h4>
   <ul className="flex flex-col gap-2 font-normal">
-    links
+    {
+      links.map((link:string)=>(<Link href='/' key={link} >{link} </Link>))
+    }
   </ul>
 </div>);
 
@@ -20,7 +28,18 @@ const Footer = ()=>{
         </div>
         <div className="flex flex-wrap gap-12">
           <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+          <div className="flex flex-1 flex-col gap-4">
+          <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+          <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+          <div className="flex flex-1 flex-col gap-4">
+          <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+          <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+          <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links} />
         </div>
+
       </div>
     </footer>
   )
