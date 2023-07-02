c383f077dc0bc559db059f675752e8af0f746338
Joffreynk <joyjoffrey@gmail.com>
2023-07-02 21:03:14 +0200

    add authOptions templates

--- lib/session.ts
+++ lib/session.ts
@@ -4,3 +4,28 @@ import { AdapterUser } from "next-auth/adapters";
 import  GoogleProvider  from "next-auth/providers/google";
 import  jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
+
+
+export const authOptions: NextAuthOptions = {
+  providers: [GoogleProvider({
+    clientId: "",
+    clientSecret: "",
+  })],
+
+  jwt: {
+    encode: ({ secret, token }) =>{},
+    decode: async({ secret, token }) =>{},
+  },
+
+  theme: {
+    colorScheme: "light",
+    logo: "/logo.png"
+  },
+
+  callbacks: {
+    async session({ session}) {},
+    async signIn({ user}) {},
+  }
+
+
+}
