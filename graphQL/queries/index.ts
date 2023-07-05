85c26037aac21bf68209e9182d1df94c1e405c94
Joffreynk <joyjoffrey@gmail.com>
2023-07-05 22:24:18 +0200

    create graphQL get and create queries

---
+++ graphQL/queries/index.ts
@@ -0,0 +1,29 @@
+export const getUserQuery = `
+  query GetUser($email: string!){
+    user(by: {email: $email}){
+      id
+      name
+      email
+      description
+      avatarUrl
+      githubUrl
+      linkedinUrl
+    }
+  }
+`;
+
+export const createUserMutation = `
+  mutation CreateUser($input: UserCreateInput!){
+    userCreate(input: $input){
+      user{
+        id
+        name
+        email
+        description
+        avatarUrl
+        githubUrl
+        linkedinUrl
+      }
+    }
+  }
+`
