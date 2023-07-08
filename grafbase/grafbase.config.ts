457439f14aeea414262eb8d2e141edab25bccba1
Joffreynk <joyjoffrey@gmail.com>
2023-07-08 23:00:14 +0200

    add rules to the schemas

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({min: 2, max:20}),
   email: g.string().unique(),
@@ -9,8 +10,11 @@ const User = g.model('User', {
   linkedInUrl: g.url().optional(),
   projects: g.relation(()=>Project).list().optional(),
 
+}).auth((rules)=>{
+  rules.public().read()
 })
 
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({min: 2}),
   description: g.string(),
@@ -19,6 +23,9 @@ const Project = g.model('Project', {
   githubUrl: g.url(),
   category: g.string().search(),
   CreatedBy: g.relation(()=>User),
+}).auth((rules)=>{
+  rules.public().read(),
+  rules.private().create().delete().update();
 })
 
 const jwt = auth.JWT({
